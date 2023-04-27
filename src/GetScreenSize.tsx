f68ead2a2acf07dfb2f25ae268ac30f546fc158a
peterkecseti <peterkecseti@gmail.com>
2023-04-27 19:23:55 +0200

    Display project made responsive for phones,tablets

---
+++ src/GetScreenSize.tsx
@@ -0,0 +1,22 @@
+/*
+0 - mobile
+1 - tablet
+2 - small desktop
+3 - anything above
+*/
+
+function GetScreenSize(){
+    const width = window.innerWidth
+    console.log(width)
+    if(width <= 480){
+        return 0
+    }
+    if(width > 481 && width <= 768){
+        return 1
+    }
+    if(width > 769 && width <= 1200){
+        return 2
+    }
+    return 3
+}
+export default GetScreenSize
---
+++ src/components/pages/DisplayProject/GenerateProjectComponents.tsx
@@ -0,0 +1,46 @@
+import arrowRight from '../../../assets/createProject/arrowRight.png'
+import arrowUp from '../../../assets/createProject/arrowUp.png'
+import { useEffect, useRef} from "react";
+
+function ProjectComponent( projectData: any, imageData: any ) {
+    const elementRef = useRef<HTMLImageElement>(null);
+    const elementHeight = useRef<number>(0);
+  
+    useEffect(() => {
+      if (elementRef.current) {
+        elementHeight.current = elementRef.current.offsetHeight;
+      }
+    }, [elementRef]);
+  
+    return (
+      <>
+        <div className="create-project-layout">
+          <div>
+            <p id="about-artwork" style={{maxHeight: `${elementHeight.current}px`}}>A few words <br /> about this artwork</p>
+            <p id="about-artwork-content">{projectData.description}</p>
+          </div>
+          <div>
+            <img ref={elementRef} src={arrowRight} alt="" id="about-arrow-right"/>
+          </div>
+          <div>
+            {
+              imageData ? <img src={imageData} style={{width: '100%'}} alt="" placeholder="image" />
+              : <div id="project-no-image-placeholder">
+                  <p className="">No image to show.</p>
+                </div>
+            }
+          </div>
+        </div>
+        <div className="create-project-layout">
+          <div></div>
+          <div></div>
+          <div className="set-artwork-title-container">
+            <img src={arrowUp} alt="" />
+            <p id="">{projectData.title}</p>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+export default ProjectComponent
